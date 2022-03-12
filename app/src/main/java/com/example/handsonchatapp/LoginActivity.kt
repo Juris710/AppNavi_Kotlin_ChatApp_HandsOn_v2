a125aa5a28098d7203844bfb01304245bb0c385d
Juris710 <juris710.dev@gmail.com>
2022-03-13 00:50:36 +0900

    Session1.4  task

--- app/src/main/java/com/example/handsonchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/handsonchatapp/LoginActivity.kt
@@ -2,10 +2,30 @@ package com.example.handsonchatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.handsonchatapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    private val TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.loginButtonLogin.setOnClickListener {
+            val email = binding.emailEdittextLogin.text.toString();
+            val password = binding.passwordEdittextLogin.text.toString();
+
+            Log.d(TAG, "Email is: $email")
+            Log.d(TAG, "password is: $password")
+        }
+        binding.backToRegisterTextLogin.setOnClickListener {
+            Log.d(TAG, "try to show register activity")
+
+            finish()
+        }
     }
 }

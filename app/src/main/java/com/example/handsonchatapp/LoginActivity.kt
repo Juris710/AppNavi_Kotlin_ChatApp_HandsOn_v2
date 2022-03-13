0cb3bef39092a0f2ff9ab9bc8194696960a6db35
Juris710 <juris710.dev@gmail.com>
2022-03-13 10:49:37 +0900

    Session2.2

--- app/src/main/java/com/example/handsonchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/handsonchatapp/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.handsonchatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.handsonchatapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -19,8 +21,30 @@ class LoginActivity : AppCompatActivity() {
             val email = binding.emailEdittextLogin.text.toString();
             val password = binding.passwordEdittextLogin.text.toString();
 
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter text in email or password", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
             Log.d(TAG, "Email is: $email")
             Log.d(TAG, "password is: $password")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isCanceled) {
+                        Log.d(TAG, "Canceled")
+                    }
+                    if (!it.isSuccessful) {
+                        Toast.makeText(this, "Failed to Login", Toast.LENGTH_SHORT).show()
+                        return@addOnCompleteListener
+                    }
+
+                    Log.d(TAG, "Successful Login")
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to Login", Toast.LENGTH_SHORT).show()
+                }
         }
         binding.backToRegisterTextLogin.setOnClickListener {
             Log.d(TAG, "try to show register activity")
